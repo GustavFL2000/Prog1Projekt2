da4ebc1fa8c168f86f699395585eda578c123340
Gustav Færmann Lassen <gustavfl13@gmail.com>
2025-10-08 10:28:45 +0200

    Opgave 1 næsten færdig mangler lige app til at teste

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,4 +1,44 @@
 package model;
 
 public class Student {
+
+    private String name;
+    private boolean active;
+    private int[] grades;
+
+    public Student() {
+        this.name = "Unknown";
+        this.active = false;
+        this.grades = new int[0];
+    }
+
+    public Student(String name, boolean active, int[] grades) {
+        this.name = name;
+        this.active = active;
+        this.grades = grades;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public int[] getGrades() {
+        return grades;
+    }
+
+    public void setGrades(int[] grades) {
+        this.grades = grades;
+    }
 }
--- src/model/Team.java
+++ src/model/Team.java
@@ -1,4 +1,62 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Team {
+
+    private String name;
+    private String room;
+    public ArrayList<Student> students = new ArrayList<>();
+
+    public Team() {
+        this.name = "Unknown";
+        this.room = "Unknown";
+    }
+
+    public Team(String name, String room) {
+        this.name = name;
+        this.room = room;
+    }
+
+    public ArrayList<Student> getActiveStudentsArrayList() {
+        ArrayList<Student> activeStudents = new ArrayList<>();
+        for (Student student : students) {
+            if (student.isActive()) {
+                activeStudents.add(student);
+            }
+        }
+        return activeStudents;
+    }
+
+    public void removeStudent(String name) {
+        int i = 0;
+        while (i < students.size()) {
+            if (students.get(i).getName().equals(name)) {
+                students.remove(i);
+                break;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    public void addStudent(Student student) {
+        this.students.add(student);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
 }
